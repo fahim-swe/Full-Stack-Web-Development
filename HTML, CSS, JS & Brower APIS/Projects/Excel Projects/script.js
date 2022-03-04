287b8846f35802516f437aa5527d8c8626034b3c
mdfahimislam <mdfahim1194@gmail.com>
2022-03-04 12:45:35 +0600

    Excell UI

--- HTML, CSS, JS & Brower APIS/Projects/Excel Projects/script.js
+++ HTML, CSS, JS & Brower APIS/Projects/Excel Projects/script.js
@@ -6,6 +6,10 @@ let col = 26;
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
 
+let addressbar = document.querySelector(".address-bar");
+
+let gridCells = document.querySelector(".cells-cont");
+
 for(let i = 0; i < row; i++)
 {
 
@@ -13,5 +17,45 @@ for(let i = 0; i < row; i++)
     addressCol.innerText = i+1;
     addressCol.setAttribute("class", "address-col");
     addressColCont.appendChild(addressCol);
+}
+
+for(let i = 0; i < col; i++)
+{
+
+    let addressCol = document.createElement("div");
+    addressCol.innerText = String.fromCharCode(65+i);
+    addressCol.setAttribute("class", "address-row");
+    addressRowCont.appendChild(addressCol);
+}
+
+
+for(let i = 0; i < row; i++)
+{
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "cell-cont");
+    for(let j = 0; j < col; j++)
+    {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        rowCont.appendChild(cell);
+
+        addListenerForAddressBarDisplay(cell, i, j);
+    }
+
+    gridCells.appendChild(rowCont);
+}
+
+
+function addListenerForAddressBarDisplay(cell, i, j)
+{
+    cell.addEventListener("click", (e) => {
+        let rowID = i+1;
+        let colID = String.fromCharCode(j+65);
+
+        addressbar.value = ` ${colID}${rowID}`;
+
+        
+    })
 }
 
