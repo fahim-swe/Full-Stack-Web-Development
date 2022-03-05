cf91c6231d5a7dd4962af6adffbbb5970d771328
mdfahimislam <mdfahim1194@gmail.com>
2022-03-06 01:45:36 +0600

    testing

--- HTML, CSS, JS & Brower APIS/Projects/Excel Projects/script.js
+++ HTML, CSS, JS & Brower APIS/Projects/Excel Projects/script.js
@@ -1,5 +1,4 @@
 
-
 let row = 100;
 let col = 26;
 
@@ -38,6 +37,12 @@ for(let i = 0; i < row; i++)
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
         cell.setAttribute("contenteditable", "true");
+
+        cell.setAttribute("spellcheck", "false");
+
+        // Attributes for cell and storage identification
+        cell.setAttribute("rid", i);
+        cell.setAttribute("cid", j);
         rowCont.appendChild(cell);
 
         addListenerForAddressBarDisplay(cell, i, j);
@@ -78,4 +83,9 @@ function addListenerForAddressBarDisplay(cell, i, j)
 
     })
 }
+
+
+// By default click on first cell
+let firstCell = document.querySelector(".cell");
+firstCell.click();
 
